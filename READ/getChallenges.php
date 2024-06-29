a90f85038e1c1e5022da299c1fd4de0231f52c01
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-06-29 13:30:41 +0200

    Adding challenges endpoints

--- READ/getChallenges.php
+++ READ/getChallenges.php
@@ -9,30 +9,40 @@ function getChallenges($conn) {
     $sql = "SELECT * FROM challenges";
     $result = $conn->query($sql);
 
-    $groups = [];
+    $challenges = [];
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            $groups[] = $row;
+            $challenges[] = $row;
         }
     } else {
         echo json_encode(['error' => 'No challenges found']);
     }
 
-    echo json_encode($groups);
+    echo json_encode($challenges);
 }
 
 function getChallengesByGroupId($conn, $params) {
     if (isset($params['id'])) {
-        $id = $conn->real_escape_string($params['id']);
-        $sql = "SELECT * FROM group_challenges WHERE group_id = $id";
-        $result = $conn->query($sql);
+        $sql = "SELECT *
+            FROM group_challenges
+            WHERE group_id = ?";
 
-        if ($result->num_rows > 0) {
-            echo json_encode($result->fetch_assoc());
+        if ($stmt = $conn->prepare($sql)) {
+            $stmt->bind_param("i", $params['id']); // "i" indicates integer type for the parameter
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            $challenges = [];
+            while ($row = $result->fetch_assoc()) {
+                $students[] = $row;
+            }
+
+            $stmt->close();
+            echo json_encode($challenges);
         } else {
-            echo json_encode(['error' => 'Group challenges not found']);
+            echo json_encode(["error" => "Failed to prepare statement"]);
         }
     } else {
-        echo json_encode(['error' => 'ID parameter missing']);  
+        echo json_encode(['error' => 'ID parameter missing']); 
     }
 }
