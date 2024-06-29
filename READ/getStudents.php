877d1d8e0e7f399be602d211b1e8e34f2017269e
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-06-29 11:54:16 +0200

    Debugging get students by group

--- READ/getStudents.php
+++ READ/getStudents.php
@@ -3,13 +3,13 @@ include 'connection.php'; // include your database connection file
 
 function getStudentsByGroup($conn, $params) {
     if (isset($params['id'])) {
-        $sql = "SELECT *
-                FROM students
-                INNER JOIN groups ON students.group_id = groups.id
-                WHERE groups.id = $params[id]";
+        $sql = "SELECT students.id, students.name, students.email
+            FROM students
+            INNER JOIN groups ON students.group_id = groups.id
+            WHERE groups.id = ?";
 
         if ($stmt = $conn->prepare($sql)) {
-            $stmt->bind_param("i", $params['id']); // bind the group ID parameter
+            $stmt->bind_param("i", $params['id']); // "i" indicates integer type for the parameter
             $stmt->execute();
             $result = $stmt->get_result();
 
@@ -19,7 +19,7 @@ function getStudentsByGroup($conn, $params) {
             }
 
             $stmt->close();
-            echo json_encode($students);
+            return $students;
         } else {
             return ["error" => "Failed to prepare statement"];
         }
