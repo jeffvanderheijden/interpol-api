ac287590356f5d997ce597d7bc6957114e190aee
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-06-30 09:54:28 +0200

    Post endpoint for creating challenges per group with generated keycodes

--- CREATE/create.php
+++ CREATE/create.php
@@ -3,9 +3,8 @@
 // Include the challenges functions file
 include 'CREATE/createChallenges.php';
 
-// Get the request URI and query string
+// Get the request URI
 $request_uri = $_SERVER['REQUEST_URI'];
-$query_string = $_SERVER['QUERY_STRING'];
 
 // Parse the URI to get the route
 $route = parse_url($request_uri, PHP_URL_PATH);
--- READ/read.php
+++ READ/read.php
@@ -7,9 +7,8 @@ include 'READ/getStudents.php';
 // Include the challenges functions file
 include 'READ/getChallenges.php';
 
-// Get the request URI and query string
+// Get the request URI
 $request_uri = $_SERVER['REQUEST_URI'];
-$query_string = $_SERVER['QUERY_STRING'];
 
 // Parse the URI to get the route
 $route = parse_url($request_uri, PHP_URL_PATH);
