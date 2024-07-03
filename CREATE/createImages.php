c9af8f59a7535763ba3a267f68908809fedcc380
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-03 16:39:36 +0200

    Refactoring to create entire team with students and image

--- CREATE/createImages.php
+++ CREATE/createImages.php
@@ -5,10 +5,10 @@ header('Content-Type: application/json');
 // Creates team image and uploads to server
 // ============================
 function createTeam($conn, $params) {
-    $response = var_dump($params);
-    echo json_encode($response);
-    die();
+    // $response = var_dump($params);
+    // echo json_encode($response);
     if (isset($params['image'])) {
+        // Save the image
         $base64_image = $params['image'];
         $base64_image = str_replace('data:image/png;base64,', '', $base64_image);
         $image_data = base64_decode($base64_image);
@@ -17,7 +17,7 @@ function createTeam($conn, $params) {
         file_put_contents($file_path, $image_data);
 
         // SQL to insert into groups table
-        $sql = "INSERT INTO groups (image_url) VALUES ('$file_path')";
+        $sql = "INSERT INTO groups (name, image_url, class) VALUES ('$params[name]', '$file_path', '$params[class]')";
 
         if ($conn->query($sql) === TRUE) {
             // If insertion into database was successful
