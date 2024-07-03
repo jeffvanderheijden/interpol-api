0dba8c1ff28ee66ebbabab5230b50c1f830cc36b
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-03 13:20:47 +0200

    Added image uploader

--- CREATE/create.php
+++ CREATE/create.php
@@ -2,6 +2,7 @@
 
 // Include the challenges functions file
 include 'CREATE/createChallenges.php';
+include 'CREATE/createImages.php';
 
 // Get the request URI
 $request_uri = $_SERVER['REQUEST_URI'];
@@ -12,6 +13,9 @@ $route = parse_url($request_uri, PHP_URL_PATH);
 switch ($route) {
     case '/api/create-challenges':
         echo createChallengesPerGroup($conn, $_POST);
+        break;
+    case '/api/upload-team-image':
+        echo createTeamImage($conn, $_POST);
         break;
     default:
         echo json_encode(['error' => 'Invalid route']);
