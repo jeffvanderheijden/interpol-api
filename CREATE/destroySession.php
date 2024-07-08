01508302d9f2a8adf075a9c6cc536b598be8d2b2
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 14:50:30 +0200

    Forgot include

--- CREATE/destroySession.php
+++ CREATE/destroySession.php
@@ -15,6 +15,7 @@ function destroySession() {
     // Destroy the session cookie
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
+        echo json_encode($params);
         setcookie(session_name(), '', time() - 42000,
             $params["path"], $params["domain"],
             $params["secure"], $params["httponly"]
--- READ/getLoggedInType.php
+++ READ/getLoggedInType.php
@@ -3,12 +3,5 @@
 session_start();
 
 function getLoggedInType() {
-   // Endpoint to check session and return session data
     echo json_encode($_SESSION['ingelogdAls']);
-    // if (isset($_SESSION['ingelogdAls']) && $_SESSION['ingelogdAls'] == 'DOCENT') {
-    //     echo true;
-    // } else {
-    //     http_response_code(401); // Unauthorized
-    //     echo json_encode(['error' => 'Not logged in as teacher']);
-    // }
 }
