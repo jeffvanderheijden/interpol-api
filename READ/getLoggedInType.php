f5b2469694ab69e12c819fd3afb787b47b685416
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 13:53:49 +0200

    Fixing bug in getting groups dashboard panel

--- READ/getLoggedInType.php
+++ READ/getLoggedInType.php
@@ -1,12 +1,8 @@
 <?php
 function getLoggedInType() {
    // Endpoint to check session and return session data
-    echo var_dump($_SESSION['ingelogdAls']);
-    die();
-    if (isset($_SESSION['ingelogdAls'])) {
-        $userData = ['inlogDocent' => $_SESSION['inlogDocent']];
-
-        echo json_encode($userData);
+    if (isset($_SESSION['ingelogdAls']) && $_SESSION['ingelogdAls'] == 'DOCENT') {
+        echo true;
     } else {
         http_response_code(401); // Unauthorized
         echo json_encode(['error' => 'Not logged in as teacher']);
