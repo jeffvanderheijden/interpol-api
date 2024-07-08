e82c31e5a52b3eeca004a4327b923d7638438c73
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 12:16:53 +0200

    Fixed access control allow origins to include all interpol subdomains

--- index.php
+++ index.php
@@ -8,27 +8,29 @@ ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
 
-header('Content-Type: application/json');
-
-// Allow from any origin
-header("Access-Control-Allow-Origin: .interpol.sd-lab.nl");
-
-// Allow specific HTTP methods
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-// Allow specific headers
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-// Handle preflight requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    // Send preflight response
-    http_response_code(204);
-    exit;
-}
-
 // ==========================================
 // END OF DELETE AFTER DEPLOYMENT !!!!!!!!!
 // ==========================================
+
+$allowedOrigins = [
+    'https://admin.interpol.sd-lab.nl',
+    'https://dashboard.interpol.sd-lab.nl',
+    'https://html-css.interpol.sd-lab.nl',
+    'https://kijk-op-de-wijk.interpol.sd-lab.nl',
+    'https://creative-coding.interpol.sd-lab.nl'
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Handle preflight request
+    exit(0);
+}
 
 // Include the database connection file
 include 'connection.php';
