759d023764031f2df703e17f7a29912efa6ad60b
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 14:55:48 +0200

    Test

--- CREATE/destroySession.php
+++ CREATE/destroySession.php
@@ -13,17 +13,17 @@ $_SESSION = array();
 
 function destroySession() {
     // Destroy the session cookie
-    // if (ini_get("session.use_cookies")) {
-    //     $params = session_get_cookie_params();
-    //     setcookie(session_name(), '', time() - 42000,
-    //         $params["path"], $params["domain"],
-    //         $params["secure"], $params["httponly"]
-    //     );
-    // }
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
 
     // Destroy the session
     session_destroy();
 
     // Return response
-    echo json_encode(['message' => 'Logged out successfully']);
+    echo 'Logged out successfully';
 }
