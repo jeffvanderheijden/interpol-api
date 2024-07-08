e5c6b91a25380950316ad4dc2b2db46624fe0473
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 14:12:03 +0200

    Debugging session

--- READ/getLoggedInType.php
+++ READ/getLoggedInType.php
@@ -4,11 +4,11 @@ session_start();
 
 function getLoggedInType() {
    // Endpoint to check session and return session data
-    echo json_encode($_SESSION);
-    if (isset($_SESSION['ingelogdAls']) && $_SESSION['ingelogdAls'] == 'DOCENT') {
-        echo true;
-    } else {
-        http_response_code(401); // Unauthorized
-        echo json_encode(['error' => 'Not logged in as teacher']);
-    }
+    echo json_encode($_SESSION['ingelogdAls']);
+    // if (isset($_SESSION['ingelogdAls']) && $_SESSION['ingelogdAls'] == 'DOCENT') {
+    //     echo true;
+    // } else {
+    //     http_response_code(401); // Unauthorized
+    //     echo json_encode(['error' => 'Not logged in as teacher']);
+    // }
 }
