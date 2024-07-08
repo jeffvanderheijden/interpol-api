b9de5e81ed81af2ca135276f73be27e8dd2048f7
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 14:51:26 +0200

    Forgot include

--- CREATE/destroySession.php
+++ CREATE/destroySession.php
@@ -13,14 +13,13 @@ $_SESSION = array();
 
 function destroySession() {
     // Destroy the session cookie
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        echo json_encode($params);
-        setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"],
-            $params["secure"], $params["httponly"]
-        );
-    }
+    // if (ini_get("session.use_cookies")) {
+    //     $params = session_get_cookie_params();
+    //     setcookie(session_name(), '', time() - 42000,
+    //         $params["path"], $params["domain"],
+    //         $params["secure"], $params["httponly"]
+    //     );
+    // }
 
     // Destroy the session
     session_destroy();
