25b1ce84ed035c99642544d364570fe192e3cb26
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-09-15 10:18:54 +0200

    fixing json_encoding issues

--- READ/getStudent.php
+++ READ/getStudent.php
@@ -4,7 +4,6 @@ function getStudent() {
     if (!isset($_SESSION['info'])) {
         return json_encode(['No student data found in session.']);
     } else {
-        var_dump($_SESSION['info']);
         $jsonData = json_encode($_SESSION['info'], JSON_PARTIAL_OUTPUT_ON_ERROR);
 
         if ($jsonData === false) {
