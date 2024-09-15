f8d3865b1a8610a22678777be3e4424f9b63e3db
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-09-15 09:26:28 +0200

    Testing connection

--- connection.php
+++ connection.php
@@ -5,6 +5,8 @@ $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
 
+echo $servername . $username . $password . $dbname;
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
