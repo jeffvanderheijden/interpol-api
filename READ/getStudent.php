cc18df298ce34b28f7deb679c054435fe2848e26
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-09-15 10:11:21 +0200

    test

--- READ/getStudent.php
+++ READ/getStudent.php
@@ -4,6 +4,7 @@ function getStudent() {
     if (!isset($_SESSION['info'])) {
         return json_encode(['No student data found in session.']);
     } else {
+        var_dump($_SESSION['info']);
         return json_encode($_SESSION['info']);
     }
 }
