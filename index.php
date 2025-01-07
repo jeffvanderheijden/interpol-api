9eeb422a2277f39bb19b76df22369033ccd1b7b1
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2025-01-07 12:58:22 +0100

    Bugfixing DELETE preflight

--- index.php
+++ index.php
@@ -29,7 +29,6 @@ if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     // Handle preflight request
-    echo $_SERVER['HTTP_ORIGIN'];
     exit(0);
 }
 
