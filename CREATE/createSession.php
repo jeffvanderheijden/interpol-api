e3c1bc5ee6c96f08de72db275238823c3c4ad2d3
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2025-01-27 13:20:06 +0100

    debugging ldap login

--- CREATE/createSession.php
+++ CREATE/createSession.php
@@ -4,8 +4,9 @@ include '_helpers/ldap.php';
 
 function createSession($params) {
     if (isset($params['username']) && isset($params['password'])) {
+        var_dump(ldap($params['username'], $params['password']));
        return ldap($params['username'], $params['password']);
     } else {
-        return json_encode(['error' => 'ID parameter missing']); 
+        return json_encode(['error' => 'ID parameter missing']);
     }
 }
